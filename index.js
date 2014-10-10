bbac75c359348e67fe4da8c1117f40cb51c28cb9
unknown <soh@APSEODESK181.aaa.ad.ea.com>
2014-10-10 21:54:55 +0900

    make cache to reduce loading time

--- index.js
+++ index.js
@@ -1,13 +1,81 @@
 var fs = require('fs');
+var crypto = require('crypto');
 var ReadOnlyData = require('./lib/readonlydata');
 
 var serializer;
 var deserializer;
 
 function require_(jsonFileName) {
+	var data = readFile(jsonFileName);
+	if (!data.fromcache) {
+		data = createFrom(data, jsonFileName);
+	}
+	return data;
+}
+
+function readFile(jsonFileName) {
 	var rawData = fs.readFileSync(jsonFileName);
-	var jsonData = JSON.parse(rawData);
-	return createFrom(jsonData, jsonFileName);
+	var sha = crypto.createHash('sha1');
+	sha.update(rawData);
+	var bufFileHash = sha.digest();
+	var bufCacheHash = new Buffer(20);
+	var fd;
+	try {
+		fd = fs.openSync(jsonFileName + '.cache', 'r');
+		fs.readSync(fd, bufCacheHash, 0, 20, 0);
+	} catch (e) {
+	}
+	var result;
+	if (bufCacheHash.toString() === bufFileHash.toString()) { // cache is valid
+		var pos = 20;
+		var bufSize = new Buffer(4);
+		fs.readSync(fd, bufSize, 0, 4, pos);
+		pos += 4;
+		var hintSize = bufSize.readUInt32LE(0);
+		var bufHintString = new Buffer(hintSize);
+		fs.readSync(fd, bufHintString, 0, hintSize, pos);
+		pos += hintSize;
+		var hints = JSON.parse(bufHintString);
+		fs.readSync(fd, bufSize, 0, 4, pos);
+		pos += 4;
+		var bufferSize = bufSize.readUInt32LE(0);
+		var buffer = new Buffer(bufferSize);
+		fs.readSync(fd, buffer, 0, bufferSize, pos);
+
+		result = new ReadOnlyData();
+		//TODO: using custom serializer with cache
+		result.init(Object.keys(hints), null, null);
+		result.hints = hints;
+		result.buffer = buffer;
+		result.cursor = buffer.length;
+		result.fromcache = true;
+		result.freeze();
+	} else {
+		result = JSON.parse(rawData);
+		Object.defineProperty(result, '__hash', {value: bufFileHash});
+		Object.defineProperty(result, '__file', {value: jsonFileName});
+	}
+	fd && fs.closeSync(fd);
+	return result;
+}
+
+function writeCache(readOnlyData, bufFileHash, jsonFileName) {
+	var hintString = JSON.stringify(readOnlyData.hints);
+	var fd = fs.openSync(jsonFileName + '.cache', 'w');
+	var pos = 0;
+	fs.writeSync(fd, bufFileHash, 0, 20, pos);
+	pos += 20;
+	var bufHints = new Buffer(4 + hintString.length);
+	bufHints.writeUInt32LE(hintString.length, 0);
+	bufHints.write(hintString,  4);
+	fs.writeSync(fd, bufHints, 0, bufHints.length, pos);
+	pos += bufHints.length;
+	var bufSize = new Buffer(4);
+	bufSize.writeUInt32LE(readOnlyData.buffer.length, 0);
+	fs.writeSync(fd, bufSize, 0, 4, pos);
+	pos += 4;
+	fs.writeSync(fd, readOnlyData.buffer, 0, readOnlyData.buffer.length, pos);
+	fs.closeSync(fd);
 }
 
 function createFrom(jsonData, jsonFileName) {
@@ -23,13 +91,16 @@ function createFrom(jsonData, jsonFileName) {
 	for (i = 0; i < len; ++i) {
 		aReadOnlyData._insert(keys[i], jsonData[keys[i]]);
 	}
-	jsonData = null;
-	keys = null;
 	aReadOnlyData.freeze();
-
 	if (jsonFileName) {
 		console.timeEnd(jsonFileName);
 	}
+	if (jsonData.__hash && jsonData.__file && !serializer && !deserializer) {
+		writeCache(aReadOnlyData, jsonData.__hash, jsonData.__file);
+	}
+	jsonData = null;
+	keys = null;
+
 	return aReadOnlyData;
 }
 
@@ -42,5 +113,6 @@ function overrideSerializer(newSerializer, newDeserializer) {
 
 exports.require = require_;
 exports.createFrom = createFrom;
+exports.readFile = readFile;
 exports.overrideSerializer = overrideSerializer;
 exports.ReadOnlyData = ReadOnlyData;
--- test/base.js
+++ test/base.js
@@ -8,15 +8,39 @@ var a = {
 	}
 };
 
+var fs = require('fs');
+var assert = require('assert');
 var readonlydata = require('../index');
+
+before(function () {
+	fs.writeFileSync('test.json', JSON.stringify(a));
+	try {
+		fs.unlinkSync('test.json.cache');
+	} catch (e) {}
+});
 
 describe('nested obejct', function () {
 	it('simple', function () {
 		var aa = readonlydata.createFrom(a);
-		var assert = require('assert');
 		assert(aa[1] === 1);
 		assert(aa.a === 'a');
 		assert.deepEqual(aa.b, {c: 'c', d: 2});
 		assert.deepEqual(Object.keys(aa), [1, 'a', 'b']);
 	});
 });
+
+describe('cache', function () {
+	it('test require', function () {
+		var json = readonlydata.require('test.json');
+		assert.deepEqual(json, a);
+	});
+	it('cache should exist', function () {
+		assert(fs.existsSync('test.json.cache'));
+	});
+	it('require again', function () {
+		var cached = readonlydata.require('test.json');
+		assert(cached.fromcache);
+		assert.deepEqual(cached, a);
+	});
+});
+
